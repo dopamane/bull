3d9dbcf70bdce97f7ebe0f772c538c5fd7ede435
dopamane <dwc1295@gmail.com>
2025-11-23 23:00:56 -0800

    Add compact size encoding and decoding

---
+++ lib/Bull/Message/CompactSize.hs
@@ -0,0 +1,25 @@
+module Bull.Message.CompactSize
+  ( getCompactSize
+  , putCompactSize
+  ) where
+
+import Data.Binary
+import Data.Binary.Get
+import Data.Binary.Put
+
+getCompactSize :: Get Integer
+getCompactSize = do
+  b <- getWord8
+  case b of
+    0xFD -> fromIntegral <$> getWord16le
+    0xFE -> fromIntegral <$> getWord32le
+    0xFF -> fromIntegral <$> getWord64le
+    _    -> return $ fromIntegral b
+
+putCompactSize :: Integer -> Put
+putCompactSize n
+  | 0          <= n && n <= 252                  = putWord8    $ fromIntegral n
+  | 253        <= n && n <= 65535                = putWord16le $ fromIntegral n
+  | 65536      <= n && n <= 4294967295           = putWord32le $ fromIntegral n
+  | 4294967296 <= n && n <= 18446744073709551615 = putWord64le $ fromIntegral n
+  | otherwise                                    = error "size out of range"
